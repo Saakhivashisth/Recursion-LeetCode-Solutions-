c40d569fa2cb4ba557d2b375c681dfe50c3b6056
Saakhivashisth <saakhi.vashisth@gmail.com>
2025-09-18 17:45:58 +0530

    Time: 1 ms (23.63%), Space: 41.5 MB (9.15%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        //base case
+        if(n==1){
+            return true;
+        }
+        if(n<=0||n%4!=0){
+            return false;
+        }
+        //recursive call
+        return isPowerOfFour(n/4);
+    }
+}
