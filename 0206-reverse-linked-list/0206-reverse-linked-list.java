a40d78a3f5deae72dc1766302af7eecdc4c7a9db
Saakhivashisth <saakhi.vashisth@gmail.com>
2026-04-06 12:36:06 +0530

    Time: 0 ms (100%), Space: 44.2 MB (74.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,19 +9,13 @@
  * }
  */
 class Solution {
-    public ListNode reverseListHelper(ListNode prev,ListNode curr){
-        if(curr==null){
-            return prev;
-        }
-        //reverse 
-        ListNode next=curr.next;
-        curr.next=prev;
-
-        
-        
-        return reverseListHelper(curr,next);
-    }
     public ListNode reverseList(ListNode head) {
-   return reverseListHelper(null,head);
+       if(head==null||head.next==null) {
+        return head;
+       }
+       ListNode newHead=reverseList(head.next);
+       head.next.next=head;
+       head.next=null;
+       return newHead;
     }
 }
